d9f0192ca5f0947d7fb2cc8d8f4887c3c7c59cdc
Yiwei Lin <s921975628@gmail.com>
2025-01-15 01:32:22 +0800

    Add more output compared to crash

--- drgn_crash.py
+++ drgn_crash.py
@@ -4,7 +4,36 @@ from drgn import FaultError, NULL, Object, cast, container_of, execscript, offse
 from drgn.helpers.common import *
 from drgn.helpers.linux import *
 
-import datetime, time
+import datetime, time, warnings
+
+def roundup(x, y):
+    return ((x + (y - 1)) / y) * y
+
+def GIGABYTES(x):
+    return x * 2**30
+
+def MEGABYTES(x):
+    return x * 2**20
+
+def node_present_pages(nid):
+    # FIXME: Consider different arch
+    return prog["node_data"][nid].node_present_pages
+
+def get_num_physpages():
+    # Reference: https://elixir.bootlin.com/linux/v6.11/source/include/linux/mm.h#L3183
+    phys_pages = 0
+    for node in for_each_online_node():
+        phys_pages += node_present_pages(node)
+    return phys_pages
+
+def get_memory_size(prog):
+    # Reference: https://github.com/crash-utility/crash/blob/master/memory.c#L14364
+    total = prog['PAGE_SIZE'] * get_num_physpages()
+    next_gig = roundup(total, GIGABYTES(1))
+    if (next_gig > 0):
+        if ((next_gig - total) <= MEGABYTES(64)):
+                total = next_gig
+    return total
 
 uts_namespace_detail = prog['init_uts_ns']
 
@@ -18,9 +47,29 @@ xtime_sec = timekeeper.xtime_sec
 date = time.ctime(xtime_sec)
 uptime = str(datetime.timedelta(seconds=int(timekeeper.ktime_sec)))
 
+cpus = len(list(for_each_possible_cpu(prog)))
+tasks = len(list(for_each_pid(prog)))
+# Include swapper here to align with crash utility. Do
+# simple check to make sure the assumption is correct
+for cpu in for_each_possible_cpu(prog):
+    comm = idle_task(prog, cpu).comm.string_().decode("utf-8")
+    if comm != f"swapper/{cpu}":
+        warnings.warn(f"Fail to find swapper task on cpu{cpu}")
+    else:
+        tasks += 1
+
+total = get_memory_size(prog)
+memory = number_in_binary_units(total)
+
+load = ", ".join([f"{v:.2f}" for v in loadavg(prog)])
+
+print(f'CPUS: {cpus}')
 print(f'DATE: {date}')
 print(f'UPTIME: {uptime}')
+print(f'LOAD AVERAGE: {load}')
+print(f'TASKS: {tasks}')
 print(f'NODENAME : {nodename}')
 print(f'RELEASE : {release}')
 print(f'VERSION : {version}')
 print(f'MACHINE : {machine}')
+print(f'MEMORY : {memory}')
